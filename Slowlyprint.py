5dc6840042babdef1dd1ef67a1830b98b35e4169
Emmanuel Roy <emmanuelroy@emmanuelroy.tech>
2020-03-04 11:11:36 -0600

    Create Slowlyprint.py

---
+++ Slowlyprint.py
@@ -0,0 +1,8 @@
+def slowprint(text):
+  text = str(text)
+  duration = 0
+  while(duration < len(text)): 
+    time.sleep(0.1)
+    print((text[duration]),end = "", flush = True)
+    duration = duration+1
+  return 
