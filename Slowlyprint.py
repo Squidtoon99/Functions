64414d0c0832124dcd35df74191a89f8c65ee7f4
Squidtoon99 <49101235+Squidtoon99@users.noreply.github.com>
2020-03-12 11:38:17 -0500

    Correct Horrible Code

--- Slowlyprint.py
+++ Slowlyprint.py
@@ -7,3 +7,11 @@ def slowprint(text):
     print((text[duration]),end = "", flush = True)
     duration = duration+1
   return 
+#Your slowly print sux
+from time import sleep
+def printslow(_input, delay=0.03):
+  for i in range(len(_input):
+    print(_input[:i+1],end='\r', flush=True)
+    sleep(delay)
+ slowprint('FooBar')
+ printslow('BarFoo')
